a892d0ea7e93b986a94453cb1633cad3fa4a2242
Fabio Carucci <garu2000@live.it>
2025-08-08 11:46:51 +0200

    feat: implement home layout with navigation, footer and basic pages

---
+++ components/navigation/Sidebar.tsx
@@ -0,0 +1,61 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import Link from "next/link";
+
+interface SidebarItem {
+  href: string;
+  children: React.ReactNode;
+}
+
+interface SidebarProps {
+  items: SidebarItem[];
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+const Sidebar = ({ items, open, onOpenChange }: SidebarProps) => {
+  return (
+    <Sheet open={open} onOpenChange={onOpenChange}>
+      <SheetContent side="left" className="p-0 transition-none">
+        <SheetHeader className="p-4 border-b">
+          <SheetTitle>Menu</SheetTitle>
+        </SheetHeader>
+        <ScrollArea className="flex flex-col overflow-y-auto h-full pb-2">
+          {items.map((item) => (
+            <Link
+              href={item.href}
+              key={item.href}
+              className="text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+              onClick={() => onOpenChange(false)}
+            >
+              {item.children}
+            </Link>
+          ))}
+          <div className="border-t">
+            <Link
+              href="/sign-in"
+              className="text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+              onClick={() => onOpenChange(false)}
+            >
+              Login
+            </Link>
+            <Link
+              href="/sign-up"
+              className="text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+              onClick={() => onOpenChange(false)}
+            >
+              Start selling
+            </Link>
+          </div>
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default Sidebar;
