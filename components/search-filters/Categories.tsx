a15615e65e14ffd70a3eceb11167ea6aa56fc10b
Fabio Carucci <garu2000@live.it>
2025-08-08 20:45:22 +0200

    feat: add categories sidebar with nested navigation and dynamic background color

--- components/search-filters/Categories.tsx
+++ components/search-filters/Categories.tsx
@@ -5,6 +5,7 @@ import { useEffect, useRef, useState } from "react";
 import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
 import { ListFilterIcon } from "lucide-react";
+import CategoriesSidebar from "./CategoriesSidebar";
 
 interface Props {
   data: CustomCategory[];
@@ -62,6 +63,12 @@ const Categories = ({ data }: Props) => {
 
   return (
     <div className="relative w-full">
+      <CategoriesSidebar
+        open={isSidebarOpen}
+        onOpenChange={setIsSidebarOpen}
+        data={data}
+      />
+
       {/* Hidden div to measure all items */}
       <div
         ref={measureRef}
@@ -105,6 +112,7 @@ const Categories = ({ data }: Props) => {
                 !isAnyHovered &&
                 "bg-white border-primary"
             )}
+            onClick={() => setIsSidebarOpen(true)}
           >
             View All
             <ListFilterIcon className="ml-2" />
---
+++ components/search-filters/CategoriesSidebar.tsx
@@ -0,0 +1,110 @@
+import { CustomCategory } from "@/types";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useState } from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+interface Props {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  data: CustomCategory[]; // TODO: remove this later
+}
+const CategoriesSidebar = ({ open, onOpenChange, data }: Props) => {
+  const router = useRouter();
+
+  const [parentCategories, setParentCategories] = useState<
+    CustomCategory[] | null
+  >(null);
+  const [selectedCategory, setSelectedCategory] =
+    useState<CustomCategory | null>(null);
+
+  // If we have parent categories, show those, otherwise show root categories
+  const currentCategories = parentCategories || data || [];
+
+  const handleOpenChange = (open: boolean) => {
+    setParentCategories(null);
+    setSelectedCategory(null);
+    onOpenChange(open);
+  };
+
+  const handleCategoryClick = (category: CustomCategory) => {
+    if (category.subcategories && category.subcategories.length > 0) {
+      setParentCategories(category.subcategories as CustomCategory[]);
+      setSelectedCategory(category);
+    } else {
+      // This is leaf category (no subcategory)
+      if (parentCategories && selectedCategory) {
+        // This is a subcategory - navigate to /category/subcategory
+        router.push(`/${selectedCategory.slug}/${category.slug}`);
+      } else {
+        // This is a main category - navigate to /category
+        if (category.slug === "all") {
+          router.push("/");
+        } else {
+          router.push(`/${category.slug}`);
+        }
+      }
+
+      handleOpenChange(false);
+    }
+  };
+
+  const handleBackClick = () => {
+    if (parentCategories) {
+      setParentCategories(null);
+      setSelectedCategory(null);
+    }
+  };
+
+  const backgroundColor = selectedCategory?.color || "white";
+
+  return (
+    <Sheet open={open} onOpenChange={handleOpenChange}>
+      <SheetContent
+        side="left"
+        className="p-0 transition-none"
+        style={{ background: backgroundColor }}
+      >
+        <SheetHeader className="p-4 border-b">
+          <SheetTitle>Categories</SheetTitle>
+          <SheetDescription className="sr-only">
+            Choose a category
+          </SheetDescription>
+        </SheetHeader>
+        <ScrollArea className="flex flex-col overflow-y-auto h-full pb-2">
+          {parentCategories && (
+            <button
+              className="flex items-center p-4 w-full text-left hover:bg-black hover:text-white text-base font-medium"
+              onClick={handleBackClick}
+            >
+              <ChevronLeftIcon className="size-4 mr-2" />
+              Back
+            </button>
+          )}
+          {currentCategories.map((category) => (
+            <button
+              key={category.slug}
+              onClick={() => handleCategoryClick(category)}
+              className="flex items-center p-4 w-full text-left hover:bg-black hover:text-white text-base font-medium justify-between cursor-pointer"
+            >
+              {category.name}
+              {category.subcategories && category.subcategories.length > 0 && (
+                <ChevronRightIcon className="size-4" />
+              )}
+            </button>
+          ))}
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default CategoriesSidebar;
