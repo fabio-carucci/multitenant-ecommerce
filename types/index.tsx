baba49c05c35c3808e69273b79b1674b17c0c954
Fabio Carucci <garu2000@live.it>
2025-08-08 18:44:26 +0200

    fix: remove duplicate Category import statement

--- types/index.tsx
+++ types/index.tsx
@@ -1,7 +1,5 @@
 import { Category } from "@/payload-types";
 
-import { Category } from "@/payload-types";
-
 export type CustomCategory = Omit<Category, "subcategories"> & {
   subcategories?: Category[];
 };
