9133372edf1d993c8ceff84eddc3ee016f951088
Fabio Carucci <garu2000@live.it>
2025-08-11 16:40:37 +0200

    fix: avoid importing client-only code on the server

--- lib/search-params/product-filters.server.ts
+++ lib/search-params/product-filters.server.ts
@@ -1,4 +1,18 @@
-import { createLoader } from "nuqs/server";
-import { productFiltersParams } from "./product-filters";
+import { sortValues } from "@/constants";
+import {
+  createLoader,
+  parseAsStringLiteral,
+  parseAsArrayOf,
+  parseAsString,
+} from "nuqs/server";
+
+const productFiltersParams = {
+  sort: parseAsStringLiteral(sortValues).withDefault("curated"),
+  minPrice: parseAsString.withOptions({ clearOnDefault: true }).withDefault(""),
+  maxPrice: parseAsString.withOptions({ clearOnDefault: true }).withDefault(""),
+  tags: parseAsArrayOf(parseAsString)
+    .withOptions({ clearOnDefault: true })
+    .withDefault([] as string[]),
+};
 
 export const loadProductFilters = createLoader(productFiltersParams);
