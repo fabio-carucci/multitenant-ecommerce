92e7bbaa15031421194ab49ab2ee51cd518dd068
Fabio Carucci <garu2000@live.it>
2025-08-11 11:04:19 +0200

    feat: implement category-based product filtering in product list component

--- modules/products/server/procedures.ts
+++ modules/products/server/procedures.ts
@@ -1,11 +1,37 @@
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { Where } from "payload";
+import z from "zod";
 
 export const productsRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async ({ ctx }) => {
-    const data = await ctx.payload.find({
-      collection: "products",
-      depth: 1, // populate "category" & "image"
-    });
-    return data;
-  }),
+  getMany: baseProcedure
+    .input(z.object({ category: z.string().nullable().optional() }))
+    .query(async ({ ctx, input }) => {
+      const where: Where = {};
+      if (input.category) {
+        const categoriesData = await ctx.payload.find({
+          collection: "categories",
+          limit: 1,
+          pagination: false,
+          where: {
+            slug: {
+              equals: input.category,
+            },
+          },
+        });
+
+        const parentCategory = categoriesData.docs[0];
+        if (parentCategory) {
+          where["category.slug"] = {
+            equals: parentCategory.slug,
+          };
+        }
+      }
+
+      const data = await ctx.payload.find({
+        collection: "products",
+        depth: 1, // populate "category" & "image"
+        where,
+      });
+      return data;
+    }),
 });
