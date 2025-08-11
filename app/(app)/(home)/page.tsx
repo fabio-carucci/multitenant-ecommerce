f5664a13800acd45a74808765fb46dfa3d8a0ceb
Fabio Carucci <148774237+fabio-carucci@users.noreply.github.com>
2025-08-11 22:36:32 +0200

    Multi tenancy setup (#10)
    * feat: implement multi-tenant architecture with Tenants collection and user roles
    
    * feat: all "category" now displays every products

--- app/(app)/(home)/page.tsx
+++ app/(app)/(home)/page.tsx
@@ -1,13 +1,30 @@
-"use client";
+import type { SearchParams } from "nuqs/server";
+import { loadProductFilters } from "@/lib/search-params/product-filters.server";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { ProductListView } from "@/components/products/ProductListView";
+import { DEFAULT_LIMIT } from "@/constants";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
+interface Props {
+  searchParams: Promise<SearchParams>;
+}
 
-const Home = () => {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.auth.session.queryOptions());
+const HomePage = async ({ searchParams }: Props) => {
+  const filters = await loadProductFilters(searchParams);
 
-  return <div>{JSON.stringify(data?.user, null, 2)}</div>;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchInfiniteQuery(
+    trpc.products.getMany.infiniteQueryOptions({
+      ...filters,
+      limit: DEFAULT_LIMIT,
+    })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ProductListView />
+    </HydrationBoundary>
+  );
 };
 
-export default Home;
+export default HomePage;
---
+++ collections/Tenants.ts
@@ -0,0 +1,53 @@
+import type { CollectionConfig } from "payload";
+
+export const Tenants: CollectionConfig = {
+  slug: "tenants",
+  admin: {
+    useAsTitle: "slug",
+  },
+  fields: [
+    {
+      name: "name",
+      type: "text",
+      required: true,
+      label: "Store name",
+      admin: {
+        description: "This is the name of the store (e.g Fabio's Store)",
+      },
+    },
+    {
+      name: "slug",
+      type: "text",
+      required: true,
+      unique: true,
+      index: true,
+      label: "Store slug",
+      admin: {
+        description:
+          "This is the subdomain of the store (e.g [slug].sellio.com)",
+      },
+    },
+    {
+      name: "logo",
+      type: "upload",
+      relationTo: "media",
+    },
+    {
+      name: "stripeAccountId",
+      type: "text",
+      required: true,
+      admin: {
+        readOnly: true,
+      },
+    },
+    {
+      name: "stripeDetailsSubmitted",
+      type: "checkbox",
+      admin: {
+        readOnly: true,
+        description:
+          "You cannot create products until you have submitted your Stripe details",
+      },
+    },
+  ],
+};
