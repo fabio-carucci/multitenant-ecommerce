0c1b5d301e55d0bd25331e582c20b8edd93c77cf
Fabio Carucci <garu2000@live.it>
2025-08-12 18:09:34 +0200

    feat: implement checkout item list with remove functionality and product details

--- modules/checkout/view/CheckoutView.tsx
+++ modules/checkout/view/CheckoutView.tsx
@@ -5,9 +5,11 @@ import { useQuery } from "@tanstack/react-query";
 import { useCart } from "../hooks/use-cart";
 import { useEffect } from "react";
 import { toast } from "sonner";
+import { generateTenantUrl } from "@/lib/utils";
+import CheckoutItem from "../components/CheckoutItem";
 
 const CheckoutView = ({ tenantSlug }: { tenantSlug: string }) => {
-  const { productIds, clearAllCarts } = useCart(tenantSlug);
+  const { productIds, clearAllCarts, removeProduct } = useCart(tenantSlug);
 
   const trpc = useTRPC();
   const { data, error } = useQuery({
@@ -24,7 +26,30 @@ const CheckoutView = ({ tenantSlug }: { tenantSlug: string }) => {
     }
   }, [error, clearAllCarts]);
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div className="lg:pt-16 pt-4 px-4 lg:px-12">
+      <div className="grid lg:grid-cols-7 gap-4 lg:gap-16">
+        <div className="lg:col-span-4">
+          <div className="border rounded-md overflow-hidden bg-white">
+            {data?.docs.map((product, index) => (
+              <CheckoutItem
+                key={index}
+                isLast={index === data.docs.length - 1}
+                imageUrl={product.image?.url}
+                name={product.name}
+                productUrl={`${generateTenantUrl(product.tenant.slug)}/products/${product.id}`}
+                tenantUrl={generateTenantUrl(product.tenant.slug)}
+                tenantName={product.tenant.name}
+                price={product.price}
+                onRemove={() => removeProduct(product.id)}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="lg:col-span-3">Checkout Sidebar</div>
+      </div>
+    </div>
+  );
 };
 
 export default CheckoutView;
