43436058f143872cc06d46be3a525a6c51542ad2
Fabio Carucci <garu2000@live.it>
2025-08-13 17:35:31 +0200

    feat: add error message UI for failed checkout state

--- modules/checkout/components/CheckoutSidebar.tsx
+++ modules/checkout/components/CheckoutSidebar.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { formatCurrency } from "@/lib/utils";
+import { CircleXIcon } from "lucide-react";
 
 interface Props {
   total: number;
@@ -31,6 +32,16 @@ const CheckoutSidebar = ({
           Checkout
         </Button>
       </div>
+      {isCanceled && (
+        <div className="p-4 flex items-center justify-center border-t">
+          <div className="bg-red-100 border border-red-400 font-medium px-4 py-3 rounded flex items-center w-full">
+            <div className="flex items-center">
+              <CircleXIcon className="size-6 mr-2 fill-red-500 text-red-100" />
+              <span>Checkout failed. Please try again.</span>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
