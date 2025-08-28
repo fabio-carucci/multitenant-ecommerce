93a55a65748a9e0cb6fcb8b4114d1afd5d2b0164
Fabio Carucci <garu2000@live.it>
2025-08-28 20:43:02 +0200

    feat: add Stripe account verification button into dashboard

--- app/(payload)/admin/importMap.js
+++ app/(payload)/admin/importMap.js
@@ -1,11 +1,13 @@
 import { TenantField as TenantField_1d0591e3cf4f332c83a86da13a0de59a } from '@payloadcms/plugin-multi-tenant/client'
 import { WatchTenantCollection as WatchTenantCollection_1d0591e3cf4f332c83a86da13a0de59a } from '@payloadcms/plugin-multi-tenant/client'
+import { default as default_e70a449818b93290dff57d7c7a148f7b } from '../../../components/stripe-verify/index'
 import { TenantSelector as TenantSelector_1d0591e3cf4f332c83a86da13a0de59a } from '@payloadcms/plugin-multi-tenant/client'
 import { TenantSelectionProvider as TenantSelectionProvider_d6d5f193a167989e2ee7d14202901e62 } from '@payloadcms/plugin-multi-tenant/rsc'
 
 export const importMap = {
   "@payloadcms/plugin-multi-tenant/client#TenantField": TenantField_1d0591e3cf4f332c83a86da13a0de59a,
   "@payloadcms/plugin-multi-tenant/client#WatchTenantCollection": WatchTenantCollection_1d0591e3cf4f332c83a86da13a0de59a,
+  "./components/stripe-verify/index#default": default_e70a449818b93290dff57d7c7a148f7b,
   "@payloadcms/plugin-multi-tenant/client#TenantSelector": TenantSelector_1d0591e3cf4f332c83a86da13a0de59a,
   "@payloadcms/plugin-multi-tenant/rsc#TenantSelectionProvider": TenantSelectionProvider_d6d5f193a167989e2ee7d14202901e62
 }
---
+++ components/stripe-verify/index.tsx
@@ -0,0 +1,11 @@
+import { Button, Link } from "@payloadcms/ui";
+
+const StripeVerify = () => {
+  return (
+    <Link href="/stripe-verify">
+      <Button>Verify your account</Button>
+    </Link>
+  );
+};
+
+export default StripeVerify;
